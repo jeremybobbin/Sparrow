339a814c20d9911ad95d7c1f75d372ca50ab0cea
Jeremy Bobbin <jeremybobbin@gmail.com>
2018-07-30 14:05:21 -0700

    Backend complete. + /widget, /data, /lead

--- Dao.js
+++ Dao.js
@@ -1,4 +1,7 @@
-let mysql = require('mysql');
+const mysql = require('mysql');
+const axios = require('axios');
+
+const config = require('./config');
 
 module.exports = class Dao {
 
@@ -10,17 +13,16 @@ module.exports = class Dao {
 
     init() {
         let sql = [
-            `CREATE TABLE IF NOT EXISTS urls ( \
-                id int NOT NULL AUTO_INCREMENT, \
-                url varchar(512), \
-                PRIMARY KEY (id) \
-             ); `,
+            'CREATE TABLE IF NOT EXISTS urls ( id INT NOT NULL AUTO_INCREMENT, userId INT NOT NULL, `url` VARCHAR(512) NOT NULL, `widget` BOOLEAN DEFAULT TRUE, `show` BOOLEAN DEFAULT TRUE, `delay` TINYINT NOT NULL DEFAULT 3, `effect` VARCHAR(255) DEFAULT \'fade\' , `location` VARCHAR(255) NOT NULL DEFAULT \'bottom-left\', `show_counters` BOOLEAN NOT NULL DEFAULT FALSE, `initial_wait` TINYINT DEFAULT 3, PRIMARY KEY(id) ); ',
             `CREATE TABLE IF NOT EXISTS leads ( \
-                id varchar(255) NOT NULL, \
+                urlId varchar(255) NOT NULL, \
+                ip varchar(512) NOT NULL, \
                 first varchar(512), \
                 last varchar(512), \
                 email varchar(512), \
-                urlId varchar(255), \
+                city varchar(255), \
+                region varchar(255), \
+                country varchar(255), \
                 time DATETIME DEFAULT CURRENT_TIMESTAMP \
             ); `,
             `CREATE TABLE IF NOT EXISTS formNames ( \
@@ -34,7 +36,12 @@ module.exports = class Dao {
                 first varchar(512), \
                 last varchar(512), \
                 email varchar(512) \
-            ); `
+            ); `,
+            `CREATE TABLE IF NOT EXISTS users ( \
+                id varchar(255) NOT NULL PRIMARY KEY, \
+                email varchar(512), \
+                password varchar(512) \
+            ); `,
         ];
 
         Promise.all(sql.map(string => this.query(string)))
@@ -53,27 +60,32 @@ module.exports = class Dao {
     getUrlId(url) {
         return this.query(`SELECT id FROM urls WHERE url = '${url}'`)
             .then(results => {
-                if (results[0]) {
-                    return results[0].id
+                if(results && results[0] && results[0].id) {
+                    return results[0].id;
                 } else {
-                    return this.query(`INSERT INTO urls (url) VALUES ('${url}')`);
+                    return false;
                 }
             });
     }
 
     setFields(url, fId, fName, lId, lName, eId, eName) {
         return this.getUrlId(url)
-            .then(urlId =>  Promise.all([
-                    this.query(`REPLACE INTO formNames (urlId, first, last, email) VALUES (${urlId}, '${fName}', '${lName}', '${eName}');`),
-                    this.query(`REPLACE INTO formIds (urlId, first, last, email) VALUES (${urlId}, '${fId}', '${lId}', '${eId}');`)
-                ])
-            );
+            .then(urlId =>  {
+                if(!urlId) return false;
+                let ids = [urlId, fId, lId, eId];
+                Promise.all([
+                    this.query(`REPLACE INTO formNames (urlId, first, last, email) VALUES (${formatSql([urlId, fName, lName, eName])});`),
+                    this.query(`REPLACE INTO formIds (urlId, first, last, email) VALUES (${formatSql([urlId, fId, lId, eId])});`)
+                ]);
+            });
     }
 
-    getFields(url) {
+    getData(url) {
         return this.query(
             `SELECT name.first AS fName, name.last AS lName, name.email \
-            AS eName,id.first AS fId, id.last AS lId, id.email AS eId \
+            AS eName,id.first AS fId, id.last AS lId, id.email AS eId, \
+            urls.widget, urls.show, urls.delay, urls.effect, urls.location, \
+            urls.show_counters, urls.initial_wait \
             FROM formNames AS name \
             JOIN urls ON name.urlId = urls.id \
             JOIN formIds AS id \
@@ -82,20 +94,55 @@ module.exports = class Dao {
         )
         .then(r => r[0])
         .then(r => {
-            return {
-                firstname: {
-                    id: r['fId'],
-                    name: r['fName']
-                },
-                lastname: {
-                    id: r['lId'],
-                    name: r['lName']
-                },
-                email: {
-                    id: r['eId'],
-                    name: r['eName']
-                },
-            };
+            return r ? {
+                widget: r.widget,
+                show: r.show,
+                delay: r.delay,
+                effect: r.effect,
+                location: r.location,
+                show_counters: r.show_counters,
+                initial_wait: r.initial_wait,
+                fields: {
+                    firstname: {
+                        id: r['fId'],
+                        name: r['fName']
+                    },
+                    lastname: {
+                        id: r['lId'],
+                        name: r['lName']
+                    },
+                    email: {
+                        id: r['eId'],
+                        name: r['eName']
+                    }
+                }
+            } : {};
         });
     }
+
+    putLead(url, ip, first, last, email) {
+        let p1 = axios.get(`http://api.ipstack.com/${ip}?access_key=${config.ipstack}`)
+            .then(r => r.data);
+        let p2 = this.getUrlId(url);
+        return Promise.all([p1, p2]).then(r => {
+            const values = formatSql([r[1], ip, first, last, email, r[0].city, r[0].region_name, r[0].country_name]);
+            return this.query(`INSERT INTO leads (urlId, ip, first, last, email, city, region, country) VALUES (${values});`);
+        });
+    }
+
+    getWidgetInfo(url) {
+        return this.query(
+            `SELECT first, last, email, city, UNIX_TIMESTAMP(time) AS time FROM leads
+            JOIN urls ON leads.urlId = urls.id
+            WHERE url = '${url}'
+            ORDER BY time DESC 
+            LIMIT 15; `
+        );
+    }
+}
+
+function formatSql(arr) {
+    return arr
+        .map(field => field ? (Number.isInteger(field) ? field : "'" + field + "'") : 'NULL')
+        .join(', ');
 }
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const conf = require('./config.js');
 const Dao = require('./Dao.js');
+const pug = require('pug');
 
 const ms = conf.mySql;
 
@@ -11,29 +12,100 @@ const dao = new Dao(ms.database, ms.user, ms.password, ms.socketPath);
 
 const app = express();
 
+app.set('view engine', 'pug')
 
+app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/test.html'));
 
-app.post('/data', (req, res) => {
-    console.log('Requesting /data');
-    let url = req.body.url;
-    dao.getFields(url)
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+
+app.get('/data', (req, res) => {
+    dao.getData(decodeURIComponent(req.query.url))
         .then(r => {
             res.json(r);
         });
 });
 
-app.post('/fields', (req, res) => {
-    console.log('Requesting /fields');
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+});
+
+app.post('/data', (req, res) => {
     let fields = req.body.fields;
-    console.log(req.body);
+    for(let prop in fields) {
+        fields[prop].id = fields[prop].id || null;
+        fields[prop].name = fields[prop].name || null;
+    }
     dao.setFields(req.body.url, fields.firstname.id, fields.firstname.name, fields.lastname.id, fields.lastname.name, fields.email.id, fields.email.name)
-        .then(() => {
+        .then(r => {
+            if (!r) return res.send('Nope.');
             res.send('Success');
         });
 });
+
+
+app.post('/lead', (req, res) => {
+    console.log('POST on /lead');
+    let r = req.body;
+    console.log(r);
+    let ip = req.ip === '::1' ? '67.204.145.178' : req.ip;
+    dao.putLead(r.url, ip, r.firstname, r.lastname, r.email);
+    res.send('Success.');
+});
+
+app.get('/widget', (req, res) => {
+    let url = decodeURIComponent(req.query.url);
+    dao.getWidgetInfo(url).then(r => {
+        if(!r[0] || !req.query.r) return res.send('Something went wrong.');
+        r = r[Math.floor(r.length * req.query.r)];
+        res.render('widget', {msg: genMessage(r.first, r.last, r.city), time: genTime(r.time)});
+    });
+});
+
+function genMessage(first, last, city) {
+    let name;
+    if(first && last) {
+        name = first + ' ' + last[0].toUpperCase() + '.';
+    } else if (first) {
+        name = first;
+    } else {
+        name = 'Someone';
+    }
+    if(city) {
+        return name + ' from ' + city;
+    }
+    return name + ' from ' + ' somewhere';
+}
+
+function genTime(time) {
+    if(!time) return 'Some time ago';
+    const dif = divRound(new Date().getTime(), 1000) - time;
+    if (dif < 60) {
+        return timeMsg(dif, 'second');
+    }
+    const min = divRound(dif, 60);
+    if(min < 60) {
+        return timeMsg(min, 'minute');
+    }
+    const hour = divRound(min, 60);
+    if(hour < 24) {
+        return timeMsg(hour, 'hour');
+    }
+    const day = divRound(hour, 24);
+    return timeMsg(day, 'day')
+}
+
+function timeMsg(count, unit) {
+    return count + ' ' + unit + (count > 1 ? 's' : '') + ' ago';
+}
+
+const divRound = (top, bot) => Math.round(top / bot);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
